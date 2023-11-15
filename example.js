cdc7cbe08ea6577c93d1da94cee208c22566120d
selciya <23cs211@skcet.ac.in>
2023-11-15 10:18:17 +0530

    javascipt linked with new

--- example.js
+++ example.js
@@ -1,11 +1,1 @@
-function addItemAfter() {
-    // Get the list
-    var itemList = document.getElementById("itemList");
-
-    // Create a new list item
-    var newItem = document.createElement("li");
-    newItem.textContent = "New Item"; // You can customize the content here
-
-    // Insert the new item after the second item (index 1)
-    itemList.insertBefore(newItem, itemList.children[1].nextSibling);
-}
+document.getElementById("Branch").innerHTML="Branching In Git"
